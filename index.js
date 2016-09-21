b78c46c71c03275c0a9fa36b394f0b513e9340dd
sel-en-ium <sel-en-ium@hotmail.com>
2016-09-21 17:57:32 -0600

    Added isArray and isObject

--- index.js
+++ index.js
@@ -5,6 +5,24 @@
   var
     utils = {};
 
+  /**
+   * Returns true if val is an Array.
+   * @param {*} val
+   * @returns {boolean} - true if val is an Array
+   */
+  utils.isArray = function (val) {
+    return Object.prototype.toString.call(val) === '[object Array]';
+  };
+
+  /**
+   * Returns true if val is an Object.
+   * @param {*} val
+   * @returns {boolean} - true if val is an Object
+   */
+  utils.isObject = function (val) {
+    return Object.prototype.toString.call(val) === '[object Object]';
+  };
+
   module.exports = utils;
 
 }());
--- test/index.js
+++ test/index.js
@@ -20,6 +20,64 @@
 
   describe("utils", function () {
 
+    describe('#isArray(val)', function () {
+      it('should exist', function () {
+        (!!utils.isArray).should.equal(true);
+        (typeof utils.isArray).should.equal('function');
+        utils.isArray.length.should.equal(1);
+      });
+      it('should return false for NaN', function () {
+        utils.isArray(Number.NaN).should.equal(false);
+      });
+      it('should return false for strings', function () {
+        utils.isArray('9').should.equal(false);
+      });
+      it('should return false for integers', function () {
+        utils.isArray(0).should.equal(false);
+      });
+      it('should return false for floats', function () {
+        utils.isArray(0.123).should.equal(false);
+      });
+      it('should return false for functions', function () {
+        utils.isArray(function () {}).should.equal(false);
+      });
+      it('should return false for objects', function () {
+        utils.isArray({}).should.equal(false);
+      });
+      it('should return true for Arrays', function () {
+        utils.isArray([]).should.equal(true);
+      });
+    });
+
+    describe('#isObject(val)', function () {
+      it('should exist', function () {
+        (!!utils.isObject).should.equal(true);
+        (typeof utils.isObject).should.equal('function');
+        utils.isObject.length.should.equal(1);
+      });
+      it('should return false for NaN', function () {
+        utils.isObject(Number.NaN).should.equal(false);
+      });
+      it('should return false for strings', function () {
+        utils.isObject('9').should.equal(false);
+      });
+      it('should return false for integers', function () {
+        utils.isObject(0).should.equal(false);
+      });
+      it('should return false for floats', function () {
+        utils.isObject(0.123).should.equal(false);
+      });
+      it('should return false for Arrays', function () {
+        utils.isObject([]).should.equal(false);
+      });
+      it('should return false for functions', function () {
+        utils.isObject(function () {}).should.equal(false);
+      });
+      it('should return true for objects', function () {
+        utils.isObject({}).should.equal(true);
+      });
+    });
+
   });
 
 }());
